8e1c68d4a1aab18322369ebca7e4aa889baa0763
Chale <chalermpun.mo@gmail.com>
2023-06-22 03:19:37 +0700

    fixed file

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -80,26 +80,6 @@ return require('packer').startup(function(use)
     end,
       })
   
-  -- Debugging
-  use {
-    "mfussenegger/nvim-dap",
-    opt = true,
-    event = "BufReadPre",
-    module = { "plugim_config.dap_config" },
-    wants = { "nvim-dap-virtual-text", "DAPInstall.nvim", "nvim-dap-ui", "nvim-dap-python", "which-key.nvim" },
-    requires = {
-      "Pocco81/DAPInstall.nvim",
-      "theHamsta/nvim-dap-virtual-text",
-      "rcarriga/nvim-dap-ui",
-      "mfussenegger/nvim-dap-python",
-      "nvim-telescope/telescope-dap.nvim",
-      { "leoluz/nvim-dap-go", module = "dap-go" },
-      { "jbyuki/one-small-step-for-vimkind", module = "osv" },
-    },
-    config = function()
-      require("core.plugin_config.dap_config").setup()
-    end,
-  }
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
