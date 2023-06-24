0cbc754d29ad76851b25041719b01a751bfecd66
Chale <chalermpun.mo@gmail.com>
2023-06-24 09:50:32 +0700

    add clear breakpoint

--- lua/core/plugin_config/dap_python.lua
+++ lua/core/plugin_config/dap_python.lua
@@ -3,3 +3,21 @@ local mason_path = vim.fn.glob(vim.fn.stdpath "data" .."/mason/")
 
 
 local dap_python = require('dap-python').setup(mason_path .. "packages/debugpy/venv/bin/python")
+
+dap.adapters.python = {
+    type = 'executable',
+    command = 'python',
+    args = { '-m', 'debugpy.adapter' },
+}
+
+-- dap.configurations.python = {
+--     {
+--         type = 'python',
+--         request = 'launch',
+--         name = 'Launch file',
+--         program = '${file}',
+--         pythonPath = function()
+--             return mason_path
+--         end,
+--     },
+-- }
