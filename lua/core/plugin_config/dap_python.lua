1a28d316f5d62879d8da8a161813d2396d88abb7
Chale <chalermpun.mo@gmail.com>
2023-06-24 09:52:37 +0700

    clean dap python

--- lua/core/plugin_config/dap_python.lua
+++ lua/core/plugin_config/dap_python.lua
@@ -1,23 +1,3 @@
 local dap = require('dap')
 local mason_path = vim.fn.glob(vim.fn.stdpath "data" .."/mason/")
-
-
 local dap_python = require('dap-python').setup(mason_path .. "packages/debugpy/venv/bin/python")
-
-dap.adapters.python = {
-    type = 'executable',
-    command = 'python',
-    args = { '-m', 'debugpy.adapter' },
-}
-
--- dap.configurations.python = {
---     {
---         type = 'python',
---         request = 'launch',
---         name = 'Launch file',
---         program = '${file}',
---         pythonPath = function()
---             return mason_path
---         end,
---     },
--- }
