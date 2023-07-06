d4705cacafd906cc471e2192bf3ebf3e08f17095
chale <chalermpun@computerlogy.com>
2023-07-07 01:09:17 +0700

    fixed scrollbar

--- lua/core/plugin_config/scrollbar.lua
+++ lua/core/plugin_config/scrollbar.lua
@@ -1,21 +1,21 @@
 local colors = require("tokyonight.colors").setup()
 
 require("scrollbar").setup({
-    handle = {
-        color = colors.bg_highlight,
-    },
-    marks = {
-        Search = { color = colors.orange },
-        Error = { color = colors.error },
-        Warn = { color = colors.warning },
-        Info = { color = colors.info },
-        Hint = { color = colors.hint },
-        Misc = { color = colors.purple },
-    }
+	handle = {
+		color = colors.bg_highlight,
+	},
+	marks = {
+		Search = { color = colors.orange },
+		Error = { color = colors.error },
+		Warn = { color = colors.warning },
+		Info = { color = colors.info },
+		Hint = { color = colors.hint },
+		Misc = { color = colors.purple },
+	},
 })
-
+require("scrollbar.handlers.search").setup({})
 require("hlslens").setup({
-       build_position_cb = function(plist, _, _, _)
-            require("scrollbar.handlers.search").handler.show(plist.start_pos)
-       end,
-    })
+	build_position_cb = function(plist, _, _, _)
+		require("scrollbar.handlers.search").handler.show(plist.start_pos)
+	end,
+})
