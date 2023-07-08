9e737988f61c506ff81b8330796768a05c616c43
chale <chalermpun@computerlogy.com>
2023-07-08 22:30:54 +0700

    update

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -151,6 +151,17 @@ local plugins = {
 			})
 		end,
 	},
+	{
+		"kylechui/nvim-surround",
+		version = "*", -- Use for stability; omit to use `main` branch for the latest features
+		event = "VeryLazy",
+		config = function()
+			require("nvim-surround").setup({
+				-- Configuration here, or leave empty to use defaults
+			})
+		end,
+	},
+	"f-person/git-blame.nvim",
 }
 
 require("lazy").setup(plugins, opts)
