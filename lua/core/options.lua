414c6170032fd3a5e25c431a077435f7624dab08
chale <chalermpun@computerlogy.com>
2023-07-23 00:12:27 +0700

    add cursor blink

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -20,6 +20,11 @@ vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.shiftround = true
 vim.opt.expandtab = true
+vim.opt.guicursor = {
+	"n-v-c:block-Cursor/lCursor-blinkwait1000-blinkon100-blinkoff100",
+	"i-ci:ver25-Cursor/lCursor-blinkwait1000-blinkon100-blinkoff100",
+	"r:hor50-Cursor/lCursor-blinkwait100-blinkon100-blinkoff100",
+}
 
 vim.cmd([[ set noswapfile ]])
 
