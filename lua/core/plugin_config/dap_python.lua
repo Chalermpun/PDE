dd8654d34ce1443b160aebfeecd483d811ed19a8
chale <chalermpun@computerlogy.com>
2023-08-11 21:16:45 +0700

    refactor

--- lua/core/plugin_config/dap_python.lua
+++
@@ -1,4 +0,0 @@
-local mason_path = vim.fn.glob(vim.fn.stdpath("data") .. "/mason/")
-local dap_python = require("dap-python").setup(mason_path .. "packages/debugpy/venv/bin/python")
-
-vim.fn.sign_define("DapBreakpoint", { text = " ", texthl = "", linehl = "", numhl = "" })
