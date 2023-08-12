bc7ff6d1ab68984985440fca95672e7581c3710d
chale <chalermpun@computerlogy.com>
2023-08-12 23:24:17 +0700

    minimal3

--- lua/core/plugin_config/BCE/wilder.lua
+++ lua/core/plugin_config/BCE/wilder.lua
@@ -1,9 +1,42 @@
-local wilder = require("wilder")
-wilder.setup({ modes = { ":", "/", "?" } })
+local gradient = {
+	"#f4468f",
+	"#fd4a85",
+	"#ff507a",
+	"#ff566f",
+	"#ff5e63",
+	"#ff6658",
+	"#ff704e",
+	"#ff7a45",
+	"#ff843d",
+	"#ff9036",
+	"#f89b31",
+	"#efa72f",
+	"#e6b32e",
+	"#dcbe30",
+	"#d2c934",
+	"#c8d43a",
+	"#bfde43",
+	"#b6e84e",
+	"#aff05b",
+}
 
+local wilder = require("wilder")
+
+for i, fg in ipairs(gradient) do
+	gradient[i] = wilder.make_hl("WilderGradient" .. i, "Pmenu", { { a = 1 }, { a = 1 }, { foreground = fg } })
+end
+
+wilder.setup({ modes = { ":", "/", "?" } })
 wilder.set_option(
 	"renderer",
 	wilder.popupmenu_renderer(wilder.popupmenu_palette_theme({
+		highlights = {
+			gradient = gradient, -- must be set
+			-- selected_gradient key can be set to apply gradient highlighting for the selected candidate.
+		},
+		highlighter = wilder.highlighter_with_gradient({
+			wilder.basic_highlighter(), -- or wilder.lua_fzy_highlighter(),
+		}),
 		-- 'single', 'double', 'rounded' or 'solid'
 		-- can also be a list of 8 characters, see :h wilder#popupmenu_palette_theme() for more details
 		border = "rounded",
@@ -15,3 +48,42 @@ wilder.set_option(
 		-- right = { " ", wilder.popupmenu_scrollbar() },
 	}))
 )
+
+wilder.set_option("pipeline", {
+	wilder.branch(
+		wilder.python_file_finder_pipeline({
+			file_command = function(ctx, arg)
+				if string.find(arg, ".") ~= nil then
+					return { "fdfind", "-tf", "-H" }
+				else
+					return { "fdfind", "-tf" }
+				end
+			end,
+			dir_command = { "fd", "-td" },
+			filters = { "cpsm_filter" },
+		}),
+		wilder.substitute_pipeline({
+			pipeline = wilder.python_search_pipeline({
+				skip_cmdtype_check = 1,
+				pattern = wilder.python_fuzzy_pattern({
+					start_at_boundary = 0,
+				}),
+			}),
+		}),
+		wilder.cmdline_pipeline({
+			fuzzy = 2,
+			fuzzy_filter = wilder.lua_fzy_filter(),
+		}),
+		{
+			wilder.check(function(ctx, x)
+				return x == ""
+			end),
+			wilder.history(),
+		},
+		wilder.python_search_pipeline({
+			pattern = wilder.python_fuzzy_pattern({
+				start_at_boundary = 0,
+			}),
+		})
+	),
+})
