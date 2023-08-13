c08544da6787837092e1cff48f659eaf997b928f
chale <chalermpun@computerlogy.com>
2023-08-14 00:59:05 +0700

    update

--- lua/core/plugin_config/bufferline.lua
+++
@@ -1,6 +0,0 @@
--- Bufferline
-require("bufferline").setup({})
-
--- Bufferline keymap
-vim.api.nvim_set_keymap("n", "<S-h>", ":BufferLineCyclePrev<CR>", { noremap = true })
-vim.api.nvim_set_keymap("n", "<S-l>", ":BufferLineCycleNext<CR>", { noremap = true })
--- lua/core/plugin_config/nightfox.lua
+++
@@ -1,6 +0,0 @@
--- Colorscheme
-vim.o.termguicolors = true
-vim.cmd([[ colorscheme carbonfox ]])
-
-
-vim.cmd([[highlight Visual guibg=#ffffff guifg=#000000]])
