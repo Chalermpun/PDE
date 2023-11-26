118f3e6fb95d37679c0036447aa824d142e53843
chale <chalermpun@computerlogy.com>
2023-11-26 16:01:06 +0700

    minimap

--- lua/core/plugins/editor.lua
+++ lua/core/plugins/editor.lua
@@ -521,4 +521,42 @@ return {
 			})
 		end,
 	},
+
+	-- minimap
+	{
+		"echasnovski/mini.map",
+		branch = "stable",
+		event = "BufWinEnter",
+		dependencies = { "dstein64/nvim-scrollview", enabled = false },
+		config = function()
+			local map = require("mini.map")
+			map.setup({
+				integrations = {
+					map.gen_integration.builtin_search(),
+					map.gen_integration.diagnostic({
+						error = "DiagnosticFloatingError",
+						warn = "DiagnosticFloatingWarn",
+						info = "DiagnosticFloatingInfo",
+						hint = "DiagnosticFloatingHint",
+					}),
+				},
+				symbols = {
+					encode = map.gen_encode_symbols.dot("4x2"),
+					scroll_line = "",
+				},
+				window = {
+					side = "right",
+					width = 18, -- set to 1 for a pure scrollbar :)
+					winblend = 15,
+					show_integration_count = false,
+					focusable = true,
+				},
+			})
+		end,
+		keys = {
+			{ "<leader>mo", "<cmd>lua require('mini.map').open()<cr>", desc = "Minimap Open" },
+			{ "<leader>mc", "<cmd>lua require('mini.map').close()<cr>", desc = "Minimap Close" },
+			{ "<leader>mf", "<cmd>lua require('mini.map').toggle_focus()<cr>", desc = "Minimap Focus" },
+		},
+	},
 }
