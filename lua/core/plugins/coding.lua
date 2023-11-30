df7ca034fa68d3c6f206b3a02cb20a9e0786fd9f
chale <chalermpun@computerlogy.com>
2023-11-30 22:29:44 +0700

    vim surround

--- lua/core/plugins/coding.lua
+++ lua/core/plugins/coding.lua
@@ -207,39 +207,27 @@ return {
 	-- to select the text inside, change or modify the surrounding characters,
 	-- and more.
 	{
-		"echasnovski/mini.surround",
+		"kylechui/nvim-surround",
+		version = "*", -- Use for stability; omit to use `main` branch for the latest features
 		event = "VeryLazy",
-		keys = function(_, keys)
-			-- Populate the keys based on the user's options
-			local plugin = require("lazy.core.config").spec.plugins["mini.surround"]
-			local opts = require("lazy.core.plugin").values(plugin, "opts", false)
-			local mappings = {
-				{ opts.mappings.add, desc = "Add surrounding", mode = { "n", "v" } },
-				{ opts.mappings.delete, desc = "Delete surrounding" },
-				{ opts.mappings.find, desc = "Find right surrounding" },
-				{ opts.mappings.find_left, desc = "Find left surrounding" },
-				{ opts.mappings.highlight, desc = "Highlight surrounding" },
-				{ opts.mappings.replace, desc = "Replace surrounding" },
-				{ opts.mappings.update_n_lines, desc = "Update `MiniSurround.config.n_lines`" },
-			}
-			mappings = vim.tbl_filter(function(m)
-				return m[1] and #m[1] > 0
-			end, mappings)
-			return vim.list_extend(mappings, keys)
+		config = function()
+			require("nvim-surround").setup({
+				keymaps = {
+					insert = "<C-g>s",
+					insert_line = "<C-g>S",
+					normal = "ys",
+					normal_cur = "yss",
+					normal_line = "yS",
+					normal_cur_line = "ySS",
+					visual = "S",
+					visual_line = "gS",
+					delete = "ds",
+					change = "cs",
+					change_line = "cS",
+				},
+			})
 		end,
-		opts = {
-			mappings = {
-				add = "gsa", -- Add surrounding in Normal and Visual modes
-				delete = "gsd", -- Delete surrounding
-				find = "gsf", -- Find surrounding (to the right)
-				find_left = "gsF", -- Find surrounding (to the left)
-				highlight = "gsh", -- Highlight surrounding
-				replace = "gsr", -- Replace surrounding
-				update_n_lines = "gsn", -- Update `n_lines`
-			},
-		},
 	},
-
 	-- comments
 	{
 		"JoosepAlviste/nvim-ts-context-commentstring",
@@ -423,30 +411,31 @@ return {
 				["<C-p>"] = actions.cycle_history_prev,
 			}
 
-		require("yanky").setup({
-			ring = {
-				history_length = 100,
-				storage = "shada",
-				sync_with_numbered_registers = true,
-				cancel_event = "update",
-			},
-			picker = {
-				telescope = {
-					use_default_mappings = false,
-					mappings = mappings,
+			require("yanky").setup({
+				ring = {
+					history_length = 100,
+					storage = "shada",
+					sync_with_numbered_registers = true,
+					cancel_event = "update",
 				},
-			},
-			system_clipboard = {
-				sync_with_ring = true,
-			},
-			highlight = {
-				on_put = true,
-				on_yank = true,
-				timer = 250,
-			},
-			preserve_cursor_position = {
-				enabled = true,
-			},
-		})
+				picker = {
+					telescope = {
+						use_default_mappings = false,
+						mappings = mappings,
+					},
+				},
+				system_clipboard = {
+					sync_with_ring = true,
+				},
+				highlight = {
+					on_put = true,
+					on_yank = true,
+					timer = 250,
+				},
+				preserve_cursor_position = {
+					enabled = true,
+				},
+			})
 		end,
-	}}
+	},
+}
