7a728f48aed77856bcaa6c6bcf68ee94c87105bb
Chalermpun <91321493+Chalermpun@users.noreply.github.com>
2026-01-19 00:28:32 +0700

    added live preview

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -69,4 +69,28 @@ return {
 			)
 		end,
 	},
+	  {
+    "brianhuster/live-preview.nvim",
+    dependencies = {
+      "nvim-telescope/telescope.nvim",
+      "ibhagwan/fzf-lua",
+      "echasnovski/mini.pick",
+      "folke/snacks.nvim",
+    },
+    config = function()
+      require("live-preview").setup()
+
+      vim.o.autowriteall = true
+
+      vim.api.nvim_create_autocmd(
+        { "InsertLeave", "TextChanged", "TextChangedI" },
+        {
+          pattern = "*",
+          callback = function()
+            vim.cmd("silent! write")
+          end,
+        }
+      )
+    end,
+  }
 }
